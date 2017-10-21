238b2a33af8ad48087025022f44c66e036c55a3f
Hailey Willis <yourFriendWes@users.noreply.github.com>
2017-10-20 17:56:23 -0700

    Add submitStar action

--- src/actions/starMessage.js
+++ src/actions/starMessage.js
@@ -1,3 +1,5 @@
+import { getRoomsChannel } from '../reducers/rooms'
+
 export const starMessage = (messageId) => ({
   type: 'STAR_MESSAGE',
   messageId: messageId
@@ -12,3 +14,8 @@ export const replaceStarMessages = (data) => ({
   type: "REPLACE_STAR_MESSAGES",
   starredMessages: data
 })
+
+export const submitStar = (messageId) => (dispatch, getState) => {
+  const rooms = getRoomsChannel(getState())
+  return rooms.push('starred_message:create', messageId)
+}
