5dc7eeed398d32bcace514306fa579ea8b915a6e
Hailey Willis <yourFriendWes@users.noreply.github.com>
2017-11-14 16:00:46 -0800

    Remove getRoom from roomReducer

--- src/reducers/rooms.js
+++ src/reducers/rooms.js
@@ -1,19 +1,4 @@
 import { getChannel } from './channels'
 
-const initialState = {}
-
-export const roomsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'UPDATE_ROOMS':
-      return action.rooms
-    default:
-      return state
-  }
-}
-
-export const getRooms = (state) => state.rooms || {}
-export const getRoom = (state, key) => getRooms(state)[key]
 export const getRoomsChannel = (state) => getChannel(state, 'rooms')
 export const getRoomChannel = (state, key) => getChannel(state, 'room:' + key)
-
-export default roomsReducer
